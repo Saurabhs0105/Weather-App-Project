f26b1487e5fbd8ec0131c86a44b75723c179cb90
Saurabh Singh <106254001+Saurabhs0105@users.noreply.github.com>
2024-02-01 19:30:14 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,7 +8,7 @@ export default function SearchBox({updateInfo}) {
 
 
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "94682a424aed09b9d13f629d76490339";
+    const API_KEY = "9bbhbhvg";
 
     let getWeatherInfo = async () => {
         try {
